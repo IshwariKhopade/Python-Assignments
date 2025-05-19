0d8f0b198f9edfc304f2176f9adedd1bb65bfff9
Ishwari Khopade <ishwarikhopade68@gmail.com>
2025-05-19 08:02:17 +0530

    Update Assignment5_3.py

--- Assignment_5/Assignment5_3.py
+++ Assignment_5/Assignment5_3.py
@@ -1,16 +1,19 @@
 # Accept number from user and print its multiplication table upto 10
 
-def main():
-
-    print("Enter a number:")
-    Value = int(input())
-
+def Table(iNo):
     i = 1
     Ans = 0
     while(i <= 10):
-        Ans = Value * i
-        print(Value,"x", i ,"=" ,Ans)
+        Ans = iNo * i
+        print(iNo,"x", i ,"=" ,Ans)
         i+=1
+
+def main():
+
+    print("Enter a number:")
+    iValue = int(input())
+
+    Table(iValue)    
 
 if __name__ == "__main__":
     main()
