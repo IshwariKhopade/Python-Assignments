528db560a033705509afeba79a8c29fb06809d64
Ishwari Khopade <ishwarikhopade68@gmail.com>
2025-05-19 08:01:39 +0530

    Update Assignment5_4.py

--- Assignment_5/Assignment5_4.py
+++ Assignment_5/Assignment5_4.py
@@ -1,14 +1,19 @@
 # Accept number from user and print its factorial using for loop
 
+def Factorial(iNo):
+    iMul = 1
+    for i in range(1, iNo + 1):
+        iMul = iMul * i
+    return iMul
+
 def main():
 
     print("Enter a number:")
-    Value = int(input())
+    iValue = int(input())
 
-    iMul = 1
-    for i in range(1,Value+1):
-        iMul = iMul * i
-    print(iMul)
+    Ret = Factorial(iValue)
+
+    print("Factorial is: ", Ret)
 
 if __name__ == "__main__":
     main()
