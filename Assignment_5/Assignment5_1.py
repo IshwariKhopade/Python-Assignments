8bff1d04a344e6e6e53460abcdf72fb89ce42972
Ishwari Khopade <ishwarikhopade68@gmail.com>
2025-05-19 08:03:10 +0530

    Update Assignment5_1.py

--- Assignment_5/Assignment5_1.py
+++ Assignment_5/Assignment5_1.py
@@ -1,12 +1,14 @@
 #Program to print numbers from 1-50 using while loop
 
-def main():
+def Display():
     i = 1
     while(i<=50):
         print(i)
         i+=1
 
+def main():
 
-
+    Display()
+    
 if __name__ == "__main__":
     main()
