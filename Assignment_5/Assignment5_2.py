4fd2de04d22498ad904329dcc516b58adc16bc25
Ishwari Khopade <ishwarikhopade68@gmail.com>
2025-05-19 08:02:43 +0530

    Update Assignment5_2.py

--- Assignment_5/Assignment5_2.py
+++ Assignment_5/Assignment5_2.py
@@ -1,14 +1,17 @@
 #Program to print sum of even numbers between 1 - 100. use a loop to find and print the sum of all even numbers from 1-100
 
-def main():
+def Sum():
     i = 1
     iSum = 0
     while(i<=100):
         if(i % 2 == 0):
             iSum = iSum + i
         i+=1
-    print("Sum of even numbers between 1-100: ",iSum)
+    return iSum
 
+def main():
+    Ret = Sum()
+    print("Sum of even numbers between 1-100: ",Ret)
 
 if __name__ == "__main__":
     main()
